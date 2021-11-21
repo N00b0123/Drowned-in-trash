819f78cab2cab5b5dbad5bb5a673f69ac1db4616
William <williamgrosseli@gmail.com>
2021-11-21 13:59:48 -0300

    Enemy Spawn and Shadows Fix

--- Assets/ACustom/Scripts/Collectable/Residue.cs
+++ Assets/ACustom/Scripts/Collectable/Residue.cs
@@ -9,6 +9,42 @@ public class Residue : MonoBehaviour, ICollectable
     [SerializeField] Wallet wallet;
     [SerializeField] TextMeshProUGUI collectUIText;
     [SerializeField] GameObject collectUI;
+    [SerializeField] float spawnRadius = 20;
+    [SerializeField] GameObject enemyPf;
+    int spawnLimit = 0;
+    Transform playerPosition;
+    float playerDistance;
+    float elapsedTime = 0.0f;
+    float secondsBetweenSpawn = 7f;
+
+
+    void Start()
+    {
+        playerPosition = FindPlayer.instance.player.transform;
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        playerDistance = Vector3.Distance(playerPosition.position, transform.position);
+        if (playerDistance < spawnRadius && elapsedTime > secondsBetweenSpawn && spawnLimit < 5)
+        {
+            elapsedTime = 0;
+            SpawnEnemy();
+            spawnLimit++;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPf, transform.position, Quaternion.identity);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, spawnRadius);
+    }
 
     public void Use()
     {
